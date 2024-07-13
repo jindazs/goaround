682fb8ce54f48e961ff37d45b1e99cc3354f4a63
jindazs <42227654+jindazs@users.noreply.github.com>
2024-07-13 14:01:01 +0900

    Initial Commit

---
+++ goaround/goaroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  goaroundApp.swift
+//  goaround
+//
+//  Created by Yuki Jin on 2024/07/13.
+//
+
+import SwiftUI
+
+@main
+struct goaroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
